8966bdf8aad373a8b8ff4b065ec8e9ea1c19b134
MaksymBabych <cadacred@gmail.com>
2024-01-08 11:50:14 +0200

    add HomeWork5 create branch

---
+++ HomeWork 5/LoggerAplication/LoggerAplication/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text;
+
+namespace LoggerAplication
+{
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Starter.Run();
+        }
+    }
+
+}
